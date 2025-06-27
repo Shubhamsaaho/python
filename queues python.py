2eb1e44812bec9741158b9e88657fa2b2c3d5fef
Shubhamsaaho <shubhamkumar77908@gmail.com>
2025-06-27 10:43:32 +0530

    Add files via upload

---
+++ queues python.py
@@ -0,0 +1,15 @@
+# Queue using list
+queue = []
+
+queue.append(10)
+queue.append(20)
+queue.append(30)
+queue.append(40)
+
+print("Queue:", queue)
+
+print("Peek:", queue[1])
+
+print("Popped:", queue.pop(3))
+
+print("Queue after pop:", queue)
