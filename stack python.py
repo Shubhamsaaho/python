c7883a158f875b7d0a61ef143c7e0d86572e22e2
Shubhamsaaho <shubhamkumar77908@gmail.com>
2025-06-27 10:44:06 +0530

    Add files via upload

---
+++ stack python.py
@@ -0,0 +1,13 @@
+#stack using list
+stack = []
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print("Stack:", stack)
+
+print("Peek:", stack[-1])
+
+print("Popped:", stack.pop())
+
+print("Stack after pop:", stack)
